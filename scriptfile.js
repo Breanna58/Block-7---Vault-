057df5f8df76f93bcb6301e36ced0fbc2479a2a9
Breanna58 <reyesbreanna58@gmail.com>
2024-12-07 18:36:24 -0800

    Add files via upload

---
+++ scriptfile.js
@@ -0,0 +1,30 @@
+/** Pseudocode:
+ * create an equation that equals to 10
+ * create an equation that equals to 40
+ * create an equation that equals to 39
+ * create a string to give the user the code 
+*/
+
+//1.  * create an equation that equals to 10
+let FirstNum = 5 * 2;
+console.log("FirstNum", FirstNum);
+//2. create an equation that equals to 40
+let SecondNum = 20+20
+console.log("SecondNum", SecondNum);
+//3. create an equation that equals to 39
+let ThirdNum = 49-10
+console.log("ThirdNum", ThirdNum);
+const message = `You have received this message because you have been chosen to open an important vault. Here is the secret combination`;
+
+    
+
+
+
+
+
+
+alert("The combination of the lock is 10-40-39");
+
+
+
+
